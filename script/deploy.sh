a4f8d5f573e73912cdba6de8a80e5230cd9447af
Dicky Giap <dickygiap@gmail.com>
2021-08-13 04:01:32 +0700

    update deploy

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ssh ubuntu@NODE.APP.SERVER.IP <<EOF
+ssh ubuntu@3.16.188.148 <<EOF
     cd ~/node-jenkins-app-example
     git pull origin master
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
