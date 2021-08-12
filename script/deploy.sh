5ebe645f7402b1ff618c89c93bab07090818f992
Dicky Giap <dickygiap@gmail.com>
2021-08-13 04:03:37 +0700

    update npm test

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ssh ubuntu@3.16.188.148 <<EOF
+ssh -tt ubuntu@3.16.188.148 <<EOF
     cd ~/node-jenkins-app-example
     git pull origin master
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
