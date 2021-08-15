eb7e11700a888273f67a8e2735da186d231432e4
Dicky Giap <dickygiap@gmail.com>
2021-08-15 23:20:28 +0700

    update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ssh -tt ubuntu@3.16.188.148 <<EOF
+ssh -v -tt ubuntu@3.16.188.148 <<EOF
     cd ~/node-jenkins-app-example
     git pull origin main
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
