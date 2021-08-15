31bdb5859562b2fab645fdf347195b65ec7d2ffa
Dicky Giap <dickygiap@gmail.com>
2021-08-16 02:06:07 +0700

    update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null ubuntu@NODE_APP_HOST <<EOF
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o GlobalKnownHostsFile=/dev/null ubuntu@${NODE_APP_HOST} <<EOF
     cd ~/node-jenkins-app-example
     git pull origin main
     curl -o-   https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh    | bash
